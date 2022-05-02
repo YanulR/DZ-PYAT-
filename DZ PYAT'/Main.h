2974b85512e8f9a20d18057dc2577d36c77e94c1
79823 <79823@LAPTOP-R8DN3J1C>
2022-05-02 22:22:00 +0300

    Добавьте файлы проекта.

---
+++ DZ PYAT'/Main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <iostream>
+using namespace std;
+bool InCheck(int num);
+void FillArr(int** arr, int n, int m);
+void OutArr(int** arr, int n, int m);
+#endif 
